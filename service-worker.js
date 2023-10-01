5f0091348465991316fc964df74a7c29d2fcdceb
Xapurri <57631114+Xapurri@users.noreply.github.com>
2023-10-01 20:03:00 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,50 @@
+let cache_version = cache_v013
+
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('my-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/about.html',
-          '/graficos.html',
-          '/about.css',
-          '/nav-style.css',
-          '/style-graficos.css',
-          '/styles-about.css',
-          '/barrios.js',
-          '/datos_graficos.js',
-          '/main.js',
-          '/mapa.js',
-          // ... añade otros archivos y recursos que quieras cachear
-        ]);
+  event.waitUntil(
+    caches.open(cache_version).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/about.html',
+        '/graficos.html',
+        '/about.css',
+        '/nav-style.css',
+        '/style-graficos.css',
+        '/styles-about.css',
+        '/barrios.js',
+        '/datos_graficos.js',
+        '/main.js',
+        '/mapa.js',
+        // ... añade otros archivos y recursos que quieras cachear
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  var cacheWhitelist = [cache_version]; // añade los nombres de caché que quieres mantener
+
+  event.waitUntil(
+      caches.keys().then((cacheNames) => {
+          return Promise.all(
+              cacheNames.map((cacheName) => {
+                  if (cacheWhitelist.indexOf(cacheName) === -1) {
+                      return caches.delete(cacheName);
+                  }
+              })
+          );
       })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  );
+});
+
+
+
